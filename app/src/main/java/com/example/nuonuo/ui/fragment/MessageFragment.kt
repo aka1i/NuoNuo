cec2ae8e3864d155b993f66da3cf3aaf5f8e5c3a
aka1i <847919557@qq.com>
2019-11-10 18:15:26 +0800

    0.3.0

--- app/src/main/java/com/example/nuonuo/ui/fragment/MessageFragment.kt
+++ app/src/main/java/com/example/nuonuo/ui/fragment/MessageFragment.kt
@@ -1,18 +1,23 @@
 package com.example.nuonuo.ui.fragment
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-import com.example.nuonuo.R
 
+import com.example.nuonuo.R
+import com.example.nuonuo.adapter.MainActivityPagerAdapter
+import kotlinx.android.synthetic.main.fragment_message.*
+import kotlinx.android.synthetic.main.fragment_message.viewPager
+import kotlinx.android.synthetic.main.tab_item.view.*
 
 class MessageFragment : Fragment() {
-
+    private val titleText = arrayOf("发送", "收到")
+    private val icons =
+        intArrayOf(R.drawable.message_receive, R.drawable.message_send)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -23,6 +28,33 @@ class MessageFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_message, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val adapter = fragmentManager?.let { MainActivityPagerAdapter(it) }
+        adapter?.addFragment(HomeFragment())
+        adapter?.addFragment(TreandsFragment())
+
+        viewPager.adapter = adapter
+
+        initTab()
+    }
+
+    private fun initTab() {
+        tabLayout.setupWithViewPager(viewPager)
+        for (i in titleText.indices) {
+            Log.d("12321321321",i.toString())
+            Log.d("12321321321",titleText[i])
+            Log.d("12321321321",resources.getDrawable(icons[i]).toString())
+            val v = LayoutInflater.from(activity).inflate(R.layout.tab_item, null)
+            v.tab_text.text = titleText[i]
+            v.tab_icon.setImageDrawable(resources.getDrawable(icons[i]))
+            val tab = tabLayout.getTabAt(i)
+            tab?.customView = v
+        }
     }
 
 }
